112d63887988523596d825ab96d89b36285cecfa
ToKaKarTsivadze <tokaqarco2001@gmail.com>
2022-11-06 01:09:31 +0400

    added modal
    modal when editing

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
     return (
       <div>
         {todos.map((todo) => {
-          
+          console.log('aa')
           return (
             <div key={todo.id}>
               <TodoItem
--- src/todoItem.js
+++ src/todoItem.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
+import Modal from './modal';
 
 const TodoItem = (props) => {
 
     const [edit, setEdit] = useState(false);
-    const [editedValue, setEditedValue] = useState("")
-  return (
+    const [editedValue, setEditedValue] = useState(props.item)
+    const [modal, setModal] = useState(false)
+
+  return (  
     <div>
         <p>
             {props.item}
         </p>
-        {edit && <> <input onChange={(e) => setEditedValue(e.target.value) }  placeholder={props.item}  /> <button onClick={ () => props.changeWord(editedValue) }>ok</button> </>}
-        <button onClick={() => props.deleteItem(props.id)} type='button' > delete </button>
-        {!edit &&  <button onClick={() => setEdit(true) } type='button' >  edit  </button>}
+        {edit && <> <input value={editedValue} onChange={(e) => setEditedValue(e.target.value) }/> <button onClick={ () => setModal(true) }>ok</button>  </>}
+    
+        {!edit && <> <button onClick={() => setEdit(true) } >  edit  </button>      <button onClick={() => props.deleteItem(props.id)} > delete </button> </>}
+        {modal && <Modal title = {props.item}   changeValue={() => props.changeWord(editedValue)} changeState = { () => {setModal(false); setEdit(false); setEditedValue(props.item)}}  />  }
     </div>
   )
 }
+
+
 
 
 export default TodoItem
